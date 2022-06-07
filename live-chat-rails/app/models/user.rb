9b0c5950e794b95603b90bbfe06aeecd21bb2a16
nyohoo <nyohoo2013@gmail.com>
2022-06-08 02:52:24 +0900

    add: バリデーション・アソシエーションの設定

--- live-chat-rails/app/models/user.rb
+++ live-chat-rails/app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :messages
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
 end
