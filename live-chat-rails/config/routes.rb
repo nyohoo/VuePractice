a3bfb53d8a341b6c3e6a2c1eed736c0dea7bd4b9
nyohoo <nyohoo2013@gmail.com>
2022-06-08 18:46:17 +0900

    add: いいね機能の実装

--- live-chat-rails/config/routes.rb
+++ live-chat-rails/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  constraints format: :json do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: 'auth/registrations'
     }
 
-  resources :messages, only: ['index']
-
+  resources :messages, only: ['index'] do
+    member do
+      resources :likes, only: ['create']
+    end
   end
+
 end
