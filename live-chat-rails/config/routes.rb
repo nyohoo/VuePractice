d0ef79420c90d6bd5601b6f4b7bc368532c7a2a0
nyohoo <nyohoo2013@gmail.com>
2022-06-08 17:56:59 +0900

    add: messagesコントローラーとindexアクションの実行

--- live-chat-rails/config/routes.rb
+++ live-chat-rails/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations: 'auth/registrations'
-  }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  constraints format: :json do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'auth/registrations'
+    }
+
+  resources :messages, only: ['index']
+
+  end
 end
