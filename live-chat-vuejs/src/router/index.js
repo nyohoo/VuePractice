450db78a8513bbe8c11a6fb40b04f149e3b4f88b
nyohoo <nyohoo2013@gmail.com>
2022-06-09 16:04:10 +0900

    add: vue.jsの基本的な使い方

--- live-chat-vuejs/src/router/index.js
+++ live-chat-vuejs/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WelcomePage from '../views/WelcomePage'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: WelcomePage
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ live-chat-vuejs/src/views/WelcomePage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <TestComponent @toggle="toggle" message="コンポーネントにデータが渡されています" />
+    <p v-if="isEnabled">こんにちは</p>
+  </div>
+</template>
+
+<script>
+import TestComponent from '../components/TestComponent.vue'
+
+export default {
+  components: {
+    TestComponent
+  },
+  data () {
+    return {
+      title: 'はじめてのVue.jsアプリです！',
+      subtitle: 'ようこそ',
+      isEnabled: true
+    }
+  },
+  computed: {
+    text () {
+      if (this.isEnabled) {
+        return 'こんにちは!'
+      } else {
+        return 'さようなら!'
+      }
+    }
+  },
+  methods: {
+    toggle () {
+      this.isEnabled = !this.isEnabled
+    }
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    font-size: 20px;
+    color: red;
+  }
+</style>
