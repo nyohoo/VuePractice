b2b7d76140e148559f9a7db568fc4019ea5d4fd8
nyohoo <nyohoo2013@gmail.com>
2022-06-10 22:00:20 +0900

    add: メッセージ一覧の表示

--- live-chat-vuejs/src/views/ChatroomPage.vue
+++ live-chat-vuejs/src/views/ChatroomPage.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="container">
     <NabvarPage />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
-import NabvarPage from '../components/NabvarPage.vue'
+import NabvarPage from '../components/NabvarPage'
+import ChatWindow from '../components/ChatWindow'
+import axios from 'axios'
 
 export default {
-  components: { NabvarPage },
+  components: { NabvarPage, ChatWindow },
+  data () {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    async getMessages () {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client: window.localStorage.getItem('client')
+          }
+        })
+
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+       
+       this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+  mounted () {
+    this.getMessages()
+  },
 }
 </script>
 
